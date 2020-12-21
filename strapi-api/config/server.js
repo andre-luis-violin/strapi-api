4b6da010eca9dfaa66cd0c7cacde27ad5c304392
Andre Violin <andreviolin@gmail.com>
2020-12-21 16:58:37 -0400

    first commit

---
+++ strapi-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd6ea7b9103c619d6d3a1227359fb978f'),
+    },
+  },
+});
---
+++ strapi-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd1341992-8d35-4684-96c1-6019e4504cf2'
+};
